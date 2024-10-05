2cc0ec4ced2438b9b207afb4b54c54912c4b852e
isrark005 <isrark005@gmail.com>
2024-10-05 15:00:43 +0530

    total number of summaries

--- 100xDocSumFRONTEND/src/components/dashboard/KiratControllers.tsx
+++ 100xDocSumFRONTEND/src/components/dashboard/KiratControllers.tsx
@@ -4,8 +4,11 @@ import { SubmissionFlagToggle } from "../../api/submissionControll";
 import { submissionFlagState } from "../../store/atom";
 import { useRecoilState } from "recoil";
 
+type KiratControllersProps = {
+  totalNumberOfEntried: number
+}
 
-export function KiratControllers() {
+export function KiratControllers({totalNumberOfEntried}: KiratControllersProps) {
   const [submissionFlag, setSubmissionFlag] = useRecoilState(submissionFlagState);
   const [checked, setChecked] = useState(submissionFlag);
 
@@ -25,11 +28,16 @@ export function KiratControllers() {
 
   console.log(checked);
   return (
-    <div className="kirat-controllers p-3 pr-0">
+    <div className="kirat-controllers p-3 pr-0 space-y-4">
       <div className="submission-switch flex justify-between items-center py-2 px-4 border rounded-lg shadow-md">
         <div className="content flex flex-col">
         <h3 className="font-mono text-[18px]">Receive Submission</h3></div>
         <SwitchBtn onChange={handleSummaryFlag} checked={checked} />
+      </div>
+      <div className="submission-switch flex justify-between items-center py-2 px-4 border rounded-lg shadow-md">
+        <div className="content flex flex-col">
+        <h3 className="font-mono text-[18px]">Total Entries: </h3></div>
+        <p>{totalNumberOfEntried}</p>
       </div>
     </div>
   );
